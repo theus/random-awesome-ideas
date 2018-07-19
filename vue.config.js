a89c9fe079b700c0dec75b4e0e4b366c1661eeb3
Matheus Falcão <theusx@gmail.com>
2018-07-18 23:28:37 -0300

    Fix link assets

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  outputDir: 'docs' // for Github pages
+  outputDir: 'docs', // for Github pages
+  baseUrl: process.env.NODE_ENV === 'production' ?
+    '/random-awesome-idea':
+    '/'
 }
